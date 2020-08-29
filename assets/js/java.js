6862f9f1430b720cb141c31f7138fdb65583ba52
Victoria Landron <viclandron@gmail.com>
2020-08-29 14:19:09 -0400

    added all the questions

--- assets/js/java.js
+++ assets/js/java.js
@@ -6,21 +6,53 @@ const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
 const choiceD = document.getElementById("D");
 
+//create questions
 var questions = [
     { 
-        q: 'Commonly used data types DO NOT include:', 
-        a: {
-            a: "1. strings",
-            b: "2. booleans",
-            c: "3. alerts",
-            d: "4. numbers"
-        }
+        question : 'Commonly used data types DO NOT include:', 
+        choiceA: "1. strings",
+        choiceB: "2. booleans",
+        choiceC: "3. alerts",
+        choiceD: "4. numbers",
+        correct: "C"   
     },
-    correctAnswer: "c"
-    { q: 'There are 365 days in a year.', a: 't' },
-    { q: 'There are 42 ounces in a pound.', a: 'f' },
-    { q: 'The Declaration of Independence was created in 1745.', a: 'f' },
-    { q: 'Bananas are vegetables.', a: 'f' }
+
+    { 
+        question : "The condition in an if/else statement is enclosed with ___________.",
+        choiceA : "1. quotes",
+        choiceB : "2. curly brackets",
+        choiceC : "3. parenthesis",
+        choiceD : "4. square brackets",
+        correct : "C"
+    },
+
+    { 
+        question : "Arrays in JavaScript ccan be used to store __________.",
+        choiceA : "1. numbers and strings ",
+        choiceB : "2. other arrays",
+        choiceC : "3. booleans",
+        choiceD : "4. all of the above",
+        correct : "D"
+     },
+
+    { 
+        question : "String values must be enclosed within _________ when being assigned to variables.",
+        choiceA : "1. commas",
+        choiceB : "2. curly brackets",
+        choiceC : "3. quotes",
+        choiceD : "4. parenthesis",
+        correct : "C"
+    },
+
+    { 
+        question : "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choiceA : "1. Javascript",
+        choiceB : "2. terminal/bash",
+        choiceC : "3. for loops",
+        choiceD : "4. console.log",
+        correct : "D"
+    }
+    
   ];
 
 function runQuiz() {
@@ -28,6 +60,6 @@ function runQuiz() {
     questions.forEach(
 
     )
-}
+} 
 
  
